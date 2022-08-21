6bb5edb0f3aa15afd2c1ae097ef9398c7865070a
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-21 22:54:28 +0530

    UC-2 Adding Elements in LinkList

---
+++ AddElementsLinkList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class AddElementsLinkList
+    {
+        Node head;
+        Node current;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+           
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                
+                node.next = this.head;
+                this.head = node;
+            }
+            Console.WriteLine("{0} data is inserted ", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
