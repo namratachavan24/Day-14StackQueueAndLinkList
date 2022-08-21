88f2a54f5daa16bb780a66d66322af10c2bdb74b
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-21 22:48:43 +0530

    UC-1 LinkList Added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+
+            foreach (int ele in list)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
